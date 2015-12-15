1d908b0e531f316f4a8ae941731a9eb9e596ce64
Walter Schlosser <wschloss@mymail.mines.edu>
2015-12-15 08:14:50 -0700

    Pass shader now shades the ships red when hit.  Test with the 1 key.

--- shaders/pass.f.glsl
+++ shaders/pass.f.glsl
@@ -1,6 +1,12 @@
 uniform sampler2D tex;
+uniform int hit;
 
 void main() {
     // Apply texture
     gl_FragColor = texture2D(tex, gl_TexCoord[0].st);
+
+    // shade red if hit
+    if (hit > 0) {
+        gl_FragColor.r += 0.3;
+    }
 }
